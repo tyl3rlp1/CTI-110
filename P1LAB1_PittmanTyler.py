9004f761a820672e0909b652d1cd430fff603ddd
tyl3rlp1 <pittmant4154@student.faytechcc.edu>
2025-02-20 15:03:34 -0500

    Add files via upload
    This is my submission for P1LAB1

---
+++ P1LAB1_PittmanTyler.py
@@ -0,0 +1,10 @@
+# Tyler Pittman
+# 02/20/2020
+# P1LAB1
+# This assignment is designed to teach basic usage of Python programming using IDLE.
+
+first_name = input('Enter your first name:')
+
+last_name = input('Enter your last name:')
+
+print('Hey', first_name, last_name,'welcome to my website!!!')
